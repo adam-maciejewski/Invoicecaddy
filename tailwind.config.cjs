da21dd2b8199c67f76843ad86b810b5c53b41204
Adam Maciejewski <adam.m@deligo.pl>
2025-06-22 09:16:08 +0200

    IC on Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3B82F6', // main blue
+        },
+        secondary: {
+          DEFAULT: '#10B981', // main green
+        },
+        accent: {
+          DEFAULT: '#FBBF24', // yellow
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}; 
